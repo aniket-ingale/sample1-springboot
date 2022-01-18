9ea7e59f14393a463db829d5568177800e55f8e9
aniket-ingale <aniket.ingale@gmail.com>
2022-01-18 18:21:41 -0500

    Work1

--- src/main/java/com/anix/sample1springboot/HelloController.java
+++ src/main/java/com/anix/sample1springboot/HelloController.java
@@ -1,18 +1,16 @@
 package com.anix.sample1springboot;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public Map<String, Object> getHello(){
-        Map returnMap = new HashMap();
-        returnMap.put("hello", "world");
-        return returnMap;
+    @GetMapping("/hello")
+    public String getHello(){
+        String returnVal = "";
+        returnVal = "{'hello':'world'}";
+
+        return returnVal;
     }
 }
